de7f6cb8ca340df7423bb9f7a18b300d123c2d68
Vigneshwaran <125852792+vigneshwaranbalamurugan@users.noreply.github.com>
2024-06-15 01:37:48 +0530

    commit

--- Client/src/Footer.js
+++
@@ -1,9 +0,0 @@
-import './App.css'
-
-const Footer = () => (
-    <footer className="footer">
-      <p>&copy; 2024 Hospital Machines Management System</p>
-    </footer>
-  );
-
-export default Footer;
--- Client/src/Header.js
+++
@@ -1,9 +0,0 @@
-import './App.css'
-
-const Header = () => (
-    <header className="header">
-      <h1>Integreted Hospital Equipment Care System</h1>
-    </header>
-  );
-
-  export default Header;
