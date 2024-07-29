b0b0ae56e80fd09cd98e3475b0aba801dad28bd2
Vigneshwaran <125852792+vigneshwaranbalamurugan@users.noreply.github.com>
2024-07-29 21:05:11 +0530

    Update App.js

--- Server/Client/src/App.js
+++ Server/Client/src/App.js
@@ -22,9 +22,7 @@ const createRoutes = (isLoggedIn) => [
         </ProtectedRoute>
       ), },
       { path: 'machines', element: (
-        <ProtectedRoute isLoggedIn={isLoggedIn}>
           <Machine />
-        </ProtectedRoute>
       ), },
       { path: 'admin', element: (
         <ProtectedRoute isLoggedIn={isLoggedIn}>
