1a6cfa0518e6837fb03cacd03e0c767ba9e1f396
Vigneshwaran <125852792+vigneshwaranbalamurugan@users.noreply.github.com>
2024-07-29 23:21:31 +0530

    Update server.js

--- Server/server.js
+++ Server/server.js
@@ -33,10 +33,10 @@ app.use('/equip',Machine_Router);
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(express.static(path.join(__dirname, '/Client/build')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '/Client/build/index.html'));
-});
+// app.use(express.static(path.join(__dirname, '/Client/build')));
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '/Client/build/index.html'));
+// });
 
 const port = process.env.PORT;
 app.listen(port, () => {
